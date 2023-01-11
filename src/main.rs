5013437e908a98373cc10900d22d90a1ced8440c
serggio <serggiopizzella@gmail.com>
2023-01-11 11:49:02 +0100

    add upload route

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,44 @@
-use offload::prelude;
-use std::{env, process};
+#![feature(proc_macro_hygiene, decl_macro)]
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
+#[macro_use] extern crate rocket;
+use rocket::config::Environment;
 
-    let config = prelude::Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing argumnets: {}", err);
-        process::exit(1);
-    });
+use std::path::Path;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
-    match offload::run(config).await {
-        Ok(_) => process::exit(0),
-        Err(err) => {
-            eprintln!("failed: {}", err);
-            process::exit(1);
-        }
-    };
+use rocket::{Data, State};
+
+#[post("/<name>", data = "<content>")]
+fn upload(name: String, content: Data, id_seed: State<IdSeed>) -> Result<String, std::io::Error>{
+	let id = id_seed.seed.load(Ordering::Relaxed);
+	id_seed.seed.fetch_add(1, Ordering::Relaxed);
+    
+	let filepath = format!("upload/{}-{}", name, id);
+
+	content.stream_to_file(Path::new(&filepath))?;
+
+	Ok(filepath)
+}
+struct IdSeed {
+	seed: AtomicUsize
+}
+
+impl IdSeed {
+	fn new(value: usize) -> IdSeed {
+		IdSeed { seed: AtomicUsize::new(value) }
+	}
+}
+
+fn main() {
+    let ip = local_ip_address::local_ip().unwrap().to_string();
+
+    let config = rocket::Config::build(Environment::Development)
+        .address(ip)
+        .port(8000)
+        .unwrap();
+
+    rocket::custom(config)
+		.manage(IdSeed::new(0))
+        .mount("/upload", routes![upload])
+        .launch();
 }
